c8233f5de8abb4fea5b9c52515a9fb9bd5647887
Rafael Garcia <rafbgarcia@gmail.com>
2022-12-27 12:02:19 -0300

    Add viewport meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <meta
+          name="viewport"
+          content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body className="antialiased font-sans bg-gray-200 overflow-hidden m-0">
         <Main />
         <NextScript />
