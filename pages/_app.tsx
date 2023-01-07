0afaf7b8635453d95859071c62f669ec10ae0f48
Rafael Garcia <rafbgarcia@gmail.com>
2023-01-07 15:44:26 -0300

    Move viewport meta to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from "@vercel/analytics/react"
 import { AppProps } from "next/app"
+import Head from "next/head"
 import React from "react"
 import "../styles/globals.css"
 // import "antd/dist/reset.css"
@@ -7,6 +8,12 @@ import "../styles/globals.css"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <React.StrictMode>
         <Component {...pageProps} />
       </React.StrictMode>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <script src="https://js.stripe.com/v3" async></script>
-        <meta
-          name="viewport"
-          content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
       </Head>
       <body className="antialiased font-sans bg-gray-200 overflow-hidden m-0">
         <Main />
